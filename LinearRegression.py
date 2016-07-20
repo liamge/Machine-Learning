2613651b40fcbc9f0e4f8417eb608b5191ffd062
Paige Enslow <paigeenslow@Paiges-MacBook-Pro-2.local>
2016-07-20 19:41:55 -0400

    Initialization of Linear Regression algorithm

---
+++ LinearRegression.py
@@ -0,0 +1,58 @@
+import numpy as np
+import numpy.linalg as la
+import csv
+import matplotlib.pyplot as plt
+
+
+class LinearRegression:
+    def fit(self,X,y):
+        self.X = X
+        self.y = y
+        self.m = len(y)
+        self.theta = 0
+        # mean and std need to be same dimensions as X
+
+    def gradientDescent(self, alpha, num_of_iterations, featureNormalize = True, plot = True):
+        Xtrans = self.X.transpose()
+        cost_history = []
+        if featureNormalize:
+            X = self.featureNormalize(self.X)
+        else:
+            X = self.X
+        for i in range(num_of_iterations):
+            hypothesis = np.dot(X,self.theta)
+            loss = hypothesis - y
+            cost = np.sum((loss**2)/(2*self.m))
+            cost_history.append(cost)
+            print('Iteration #%d\tCost:%d' % (i, cost))
+            gradient = np.dot(Xtrans, loss)/self.m
+            self.theta = self.theta - alpha * gradient
+        if plot:
+            plt.figure(0)
+            plt.plot(cost_history, 'r')
+            plt.ylabel('Cost')
+            plt.xlabel('Iterations')
+            plt.show()
+            plt.figure(1)
+            plt.plot(X, y,'o')
+            plt.plot(X,np.dot(X, self.theta))
+            plt.show()
+        return self.theta
+
+    def featureNormalize(self, X):
+        normalizedX = np.zeros(self.X.shape)
+        # X[0] gives first row of matrix
+        for column in range(len(X[0])):
+            mn = np.array(np.mean(X[:,column]))
+            std = np.array(np.std(X[:,column]))
+            a = X[:,column] - mn
+            normalizedX[:,[column]] = np.divide(a,std)
+        return normalizedX
+
+
+    def normalEquation(self):
+        self.theta = np.dot(np.dot(la.inv(np.dot(np.transpose(self.X), self.X)), np.transpose(self.X)),self.y)
+        return self.theta
+
+    def predict(self,x):
+        pass
